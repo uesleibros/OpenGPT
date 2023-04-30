63b192ea9d8f2b98d57fdf6d14798b381df45231
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-30 00:38:36 -0300

    Testing

---
+++ tests/forefront.py
@@ -0,0 +1,13 @@
+from unfinished.forefront.model import Model
+from unfinished.forefront.tools.system.email_creation import Email
+
+email = Email()
+val = email.CreateAccount()
+
+token = val.token
+session_id = val.sessionID
+
+forefront = Model(token=token, session_id=session_id, model="gpt-4")
+forefront.SetupConversation("Better Minecraft soundtrack's")
+
+print(forefront.SendConversation())
